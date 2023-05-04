807e76bf1d1686243326afd0ecfda116afd906c8
jiewei <jiewei@junyanginfo.com>
2023-05-04 10:52:28 +0800

    add lucky users

--- junyang_spider/libs/mail.py
+++ junyang_spider/libs/mail.py
@@ -5,9 +5,16 @@ class Mail:
     def __init__(self):
         self._server = yagmail.SMTP(user='jiewei1989830@sina.com', password='bf3fd9b21d632dd7', host='smtp.sina.com')
         self._recipients = ["jiewei@junyanginfo.com", "fanrenjing@junyanginfo.com", "zhongyuan@junyanginfo.com"]
+        self._recipients1 = ["jiewei@junyanginfo.com","cxjg168@sina.com"]
 
     def send_to(self, subject="just from endaqa", contents="default content"):
         try:
             self._server.send(to=self._recipients, subject=subject, contents=contents)
+        except BaseException as e:
+            print("send mail failed! ", e)
+
+    def send_to_contains_attach(self, attachments, subject="just from endaqa", contents="default content", ):
+        try:
+            self._server.send(to=self._recipients1, subject=subject, contents=contents, attachments=attachments)
         except BaseException as e:
             print("send mail failed! ", e)
